e1f4dc32118556d071e4bfe225b76a509c9544d8
BruceRek-law <brucewalker.dev@gmail.com>
2022-05-27 16:34:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const {Engine, World, Render, Bodies,Runner} = Matter;
-const CELLS =3;
+const {Engine, World, Render, Bodies,Runner,Body, Events} = Matter;
+const CELLS =4;
 //World's Dimesions
 const Width   =600;
 const Height  =600;
 
 const unitLength = Height/CELLS;
 const engine  = Engine.create();
+engine.world.gravity.y = 0;
 const {world} = engine;
 const render  = Render.create({
     element: document.body,
@@ -21,10 +22,10 @@ Runner.run(Runner.create(), engine);
 
 //Walls
 const walls = [
-    Bodies.rectangle(Width/2,0,Width,40,      {isStatic: true}),
-    Bodies.rectangle(Width/2,Height,Width,40, {isStatic: true}),
-    Bodies.rectangle(0,Height/2,40,Height,    {isStatic: true}),
-    Bodies.rectangle(Width,Height/2,40,Height,{isStatic: true}),
+    Bodies.rectangle(Width/2,0,Width,1,      {isStatic: true}),
+    Bodies.rectangle(Width/2,Height,Width,1, {isStatic: true}),
+    Bodies.rectangle(0,Height/2,1,Height,    {isStatic: true}),
+    Bodies.rectangle(Width,Height/2,1,Height,{isStatic: true}),
 ]
 World.add(world,walls);
 
@@ -114,7 +115,7 @@ Hori.forEach((row,columnIndex) =>{
         
         const wall = Bodies.rectangle(
             columnIndex*unitLength+(unitLength/2),
-            row*unitLength+unitLength,
+            rowIndex*unitLength+unitLength,
             unitLength,
             1,
             {
@@ -125,4 +126,64 @@ Hori.forEach((row,columnIndex) =>{
         World.add(world,wall);
     })
 })
-   
+Vert.forEach((row, rowIndex) =>{
+    row.forEach((open,columnIndex) =>{
+        if(open)
+            return;
+        
+        const wall = Bodies.rectangle(
+            columnIndex*unitLength+unitLength,
+            rowIndex*unitLength+(unitLength/2),
+            1,
+            unitLength,
+            {
+                isStatic: true
+            }
+        )
+        World.add(world,wall)
+    })
+})
+
+const goal = Bodies.rectangle(
+    Width-unitLength/2,
+    Width-unitLength/2,
+    unitLength/2,
+    unitLength/2,
+    {
+        isStatic:true,
+        label: 'goal'
+    }
+);
+
+const ball = Bodies.circle(
+    unitLength/2,
+    unitLength/2,
+    unitLength/4,
+    {
+    label: 'ball'
+    }
+)
+const objects = [goal,ball]
+World.add(world,objects);
+
+document.addEventListener('keydown', event=>{
+    let {x,y} = ball.velocity;
+
+    console.log(event)
+    if(event.keyCode === 38)
+        Body.setVelocity(ball, {x, y:y-5})
+    if(event.keyCode === 40)
+        Body.setVelocity(ball, {x, y:y+5})
+    if(event.keyCode ===37)
+        Body.setVelocity(ball, {x :x-5, y})
+    if(event.keyCode ===39)
+        Body.setVelocity(ball, {x :x+5, y})
+})
+
+//Win Condition
+Events.on(engine,'collisionStart', (event) =>{
+    event.pairs.forEach((collision) =>{
+        console.log(collision);
+    })
+    console.log(event);
+})
